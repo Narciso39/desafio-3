f145e9c7fc20893e6c1a19e80e97332e2c24f83e
luis <felipenarciso15@gmail.com>
2024-10-10 12:05:04 -0300

    feat: consumo da api do back-end ok

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,25 @@
-import React from 'react'
-import './App.css'
+// import React from 'react';
+import './App.css';
+import { useApiUser } from './hooks/useApiUser';
 
 function App() {
+  const { user: users } = useApiUser();
 
   return (
-    <>
     <div>
-      
+      <ul>
+        {users && users.length > 0 ? (
+          users.map((u) => (
+            <li key={u.id}>
+              {`${u.email} ${u.password} - nome: ${u.name}`}
+            </li>
+          ))
+        ) : (
+          <li>Nenhum usuário encontrado.</li> 
+        )}
+      </ul>
     </div>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/hooks/useApiUser.js
@@ -0,0 +1,30 @@
+import api from "../services/api";
+import { useEffect, useState } from "react";
+
+export const useApiUser = () => {
+  const [user, setUser] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    let isMounted = true;
+    api
+      .get("/all")
+      .then((response) => {
+        if (isMounted) {
+          setUser(response.data);
+        }
+      })
+      .catch((err) => {
+        if (isMounted) {
+          console.error("Ops! Ocorreu um erro: " + err);
+          setError(err);
+        }
+      });
+
+    return () => {
+      isMounted = false;
+    };
+  }, []);
+
+  return { user, error };
+};
