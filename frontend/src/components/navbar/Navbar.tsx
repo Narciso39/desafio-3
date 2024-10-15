60633926b6dbab03b1dd4b99a9aec0535e7eb1df
felipe <felipenarciso15@gmail.com>
2024-10-14 22:38:57 -0300

    feat: navbar do desktop pronta

--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -1,6 +1,13 @@
 import { NavLink } from "react-router-dom";
+//css
 import styles from "./Navbar.module.css";
+//logo
 import logo from "../../assets/navbar/logo.svg";
+//icones
+import likeIcon from "../../assets/navbar/likeIcon.svg"
+import searchIcon from "../../assets/navbar/searchIcon.svg"
+import userIcon from "../../assets/navbar/userIcon.svg"
+import carIcon from "../../assets/navbar/carIcon.svg"
 
 const Navbar = () => {
   return (
@@ -9,7 +16,7 @@ const Navbar = () => {
         <NavLink to="/">
             <img src={logo} alt="Furniro logo" />
           </NavLink>
-          <NavLink to="/">Furniro</NavLink>
+          <NavLink to="/"><h1>Furniro</h1></NavLink>
           </div>
       <ul className={styles.ul}>
         <li>
@@ -23,6 +30,20 @@ const Navbar = () => {
         </li>
         <li>
           <NavLink to="/contact">Contact</NavLink>
+        </li>
+      </ul>
+      <ul className={styles.icons}>
+        <li>
+          <img src={userIcon} alt="User" />
+        </li>
+        <li>
+        <img src={searchIcon}alt="search" />
+        </li>
+        <li>
+        <img src={likeIcon} alt="liked" />
+        </li>
+        <li>
+        <img src={carIcon} alt="car" />
         </li>
       </ul>
     </nav>
