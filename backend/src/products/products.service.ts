ad4b8aff0a1a242d687b7dd7dfe3f15408e3aee7
luis <felipenarciso15@gmail.com>
2024-10-16 15:48:31 -0300

    feat: iniciei a api

--- backend/src/products/products.service.ts
+++ backend/src/products/products.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateProductDTO } from './dto/create-product-dto';
 
 @Injectable()
 export class ProductsService {
-    constructor(private readonly prisma: ProductsService) {
+    constructor(private readonly prisma: PrismaService) {
 
+        // add({algo}:CreateProductDTO) {
+        //     this.prisma.product.create({
+        //         data: {
+        //             algo,
+        //         }
+        //     })
+        // }
 }
 }
