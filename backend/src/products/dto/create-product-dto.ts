de6fd8418e15ff3f299ea9c812fad655d651077e
felipe <felipenarciso15@gmail.com>
2024-10-16 11:02:56 -0300

    refactor: products

---
+++ backend/src/products/dto/create-product-dto.ts
@@ -0,0 +1,16 @@
+import { IsEmail, IsString, IsStrongPassword } from 'class-validator';
+
+export class CreateProductDTO {
+//   @IsString()
+//   name: string;
+
+//   @IsEmail()
+//   email: string;
+
+//   @IsStrongPassword({
+//     minLength: 6,
+//     minLowercase: 1,
+//     minUppercase: 1,
+//   })
+//   password: string;
+}
