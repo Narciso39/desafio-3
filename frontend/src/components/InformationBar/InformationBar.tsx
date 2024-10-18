fd38fc09f27e1133654fc8894be375bf0d7312b4
luis <felipenarciso15@gmail.com>
2024-10-18 15:47:21 -0300

    feat: comecei a barra de informação

---
+++ frontend/src/components/InformationBar/InformationBar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from "./InformationBar.module.css";
+const InformationBar: React.FC = () => {
+  return (
+    <section>
+
+    </section>
+  )
+}
+
+export default InformationBar
