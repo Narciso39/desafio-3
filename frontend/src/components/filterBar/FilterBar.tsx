8d35dc7c0f0a6199ea7f7e64602a97da98ee6163
luis <felipenarciso15@gmail.com>
2024-10-22 11:13:59 -0300

    feat: estilização pronta, falta as funcionalidades

--- frontend/src/components/filterBar/FilterBar.tsx
+++ frontend/src/components/filterBar/FilterBar.tsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import styles from "./FilterBar.module.css"
-
+import filter from "../../assets/filterBar/filtersvg.svg"
+import square from "../../assets/filterBar/squaresvg.svg"
+import dimension from "../../assets/filterBar/dimensionsvg.svg"
+import pipe from  "../../assets/pipeBar/pipe.svg"
 const FilterBar: React.FC = () => {
   return (
     <section className={styles.filter}>
         <dl>
-            <dd><img src="" alt="" /><span>Filter</span></dd>
-            <dd><img src="" alt="" /></dd>
-            <dd><img src="" alt="" /></dd>
-            <dd><img src="" alt="" /></dd>
+            <dd><img src={filter} alt="" /></dd>
+            <dd><span>Filter</span></dd>
+            <dd><img src={square} alt="" /></dd>
+            <dd><img src={dimension} alt="" /></dd>
+            <dd><img src={pipe} alt="" /></dd>
             <dd>Showing 1-16 of 32 results</dd>
         </dl>
         <form>
@@ -18,6 +22,7 @@ const FilterBar: React.FC = () => {
           type="text"
           id="input1"
           value="16"
+          className={styles.first}
         />
       </div>
       <div>
@@ -26,6 +31,7 @@ const FilterBar: React.FC = () => {
           type="text"
           id="input2"
           value="Default"
+          className={styles.last}
         />
       </div>
         </form>
