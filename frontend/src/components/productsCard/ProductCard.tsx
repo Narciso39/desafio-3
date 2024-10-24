f8cb98e624532bb16d0019ac99d1b09db221db35
felipe <felipenarciso15@gmail.com>
2024-10-24 19:00:18 -0300

    feat: fiz o hover do card de produto

--- frontend/src/components/productsCard/ProductCard.tsx
+++ frontend/src/components/productsCard/ProductCard.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import { useNavigate } from "react-router-dom"; // useNavigate em vez de useRouter
 import styles from "./ProductCard.module.css";
 import { Product } from "../../types/Product";
-
-
+import compare from '../../assets/icons/compare.svg'
+import like from '../../assets/icons/like.svg'
+import share from '../../assets/icons/gridicons_share.svg'
 interface ProductCardProps {
   products: Product[];
   limit: number;
@@ -10,16 +12,20 @@ interface ProductCardProps {
 
 const ProductCard: React.FC<ProductCardProps> = ({ products, limit }) => {
   const displayedProducts = products.slice(0, limit);
+  const navigate = useNavigate(); // Hook para navegação
 
   const calculateDiscountPercentage = (price: number, discountPrice?: number) => {
     if (!discountPrice) return 0;
     return ((price - discountPrice) / price) * 100;
   };
 
+  const handleSeeDetails = (id: number) => {
+    navigate(`/product/${id}`);
+  };
+
   return (
     <div className={styles.productCardContainer}>
       {displayedProducts.map((product) => {
-        // Conversão de strings para números
         const numericPrice = Number(product.price);
         const numericDiscountPrice = product.discount_price ? Number(product.discount_price) : undefined;
 
@@ -33,21 +39,43 @@ const ProductCard: React.FC<ProductCardProps> = ({ products, limit }) => {
                 </span>
               )}
             </div>
-            <img src={product.image_link} alt={product.name} className={styles.productImage} />
-            <h3 className={styles.productTitle}>{product.name}</h3>
-            <p className={styles.productDescription}>{product.category.name}</p>
-            <p className={styles.productPrice}>
-              {numericDiscountPrice ? (
-                <>
-                  <span className={styles.oldPrice}>
-                    ${numericPrice.toFixed(2)}
-                  </span>
-                  <span>${numericDiscountPrice.toFixed(2)}</span>
-                </>
-              ) : (
-                <span>${numericPrice.toFixed(2)}</span>
-              )}
-            </p>
+
+            <div className={styles.cardContent}>
+              <img src={product.image_link} alt={product.name} className={styles.productImage} />
+              <h3 className={styles.productTitle}>{product.name}</h3>
+              <p className={styles.productDescription}>{product.category.name}</p>
+              <p className={styles.productPrice}>
+                {numericDiscountPrice ? (
+                  <>
+                    <span className={styles.oldPrice}>
+                      ${numericPrice.toFixed(2)}
+                    </span>
+                    <span>${numericDiscountPrice?.toFixed(2)}</span>
+                  </>
+                ) : (
+                  <span>${numericPrice.toFixed(2)}</span>
+                )}
+              </p>
+            </div>
+
+            {/* Overlay de hover */}
+            <div className={styles.hoverOverlay}>
+              <button className={styles.seeDetailsBtn} onClick={() => handleSeeDetails(product.id)}>
+                See Details
+              </button>
+
+              <div className={styles.iconButtons}>
+                <button className={styles.iconBtn}>
+                  <img src={share} alt="" />Share
+                </button>
+                <button className={styles.iconBtn}>
+                <img src={compare} alt="" />Compare
+                </button>
+                <button className={styles.iconBtn}>
+                <img src={like} alt="" />Like
+                </button>
+              </div>
+            </div>
           </article>
         );
       })}
