4ed3a02d15c5670b0d9aa2b02609a962d2782fcb
felipe <felipenarciso15@gmail.com>
2024-10-26 03:06:48 -0300

    feat: projeto funcionando corretamente

--- frontend/src/components/ShowMoreBTN/ShowMore.tsx
+++ frontend/src/components/ShowMoreBTN/ShowMore.tsx
@@ -1,30 +1,41 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom"; // Alterado aqui
+import { useNavigate } from "react-router-dom";
 import styles from './ShowMore.module.css';
 
-const ShowMore: React.FC<{ onShowMore: () => void , use: boolean}> = ({ onShowMore, use }) => {
+const ShowMore: React.FC<{ onShowMore: () => void, use: boolean }> = ({ onShowMore, use }) => {
   const [clickCount, setClickCount] = useState(0);
-  const navigate = useNavigate(); // Alterado aqui
+  const navigate = useNavigate();
 
   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    event.preventDefault(); // Previne o redirecionamento padrão do NavLink
-    setClickCount((prevCount) => prevCount + 1);
+    event.preventDefault();
 
-    if (clickCount === 0) {
-      onShowMore(); // Chama a função para mostrar mais produtos
-    } else if (clickCount === 1) {
-      navigate("/shop"); // Alterado aqui para usar navigate
-    }
+    // Aumenta o contador baseado no estado anterior
+    setClickCount((prevCount) => {
+      const newCount = prevCount + 1;
+
+      // Verifica o novo contador e executa a ação apropriada
+      if (newCount === 1) {
+        onShowMore(); // Chama a função para mostrar mais produtos
+      } else if (newCount === 2) {
+        navigate("/shop"); 
+      }
+
+      return newCount; // Retorna o novo contador
+    });
   };
 
   return (
     <>
-    {!use && (<button className={styles.a} onClick={handleClick}>
-      Show More
-    </button>)}
-    {use && (<button className={styles.a} onClick={() => navigate("/shop")}>
-      Show More
-    </button>)}
+      {!use && (
+        <button className={styles.a} onClick={handleClick}>
+          Show More
+        </button>
+      )}
+      {use && (
+        <button className={styles.a} onClick={() => navigate("/shop")}>
+          Show More
+        </button>
+      )}
     </>
   );
 };
---
+++ frontend/src/services/Firebase.ts
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBAgaxt7Ef2-D3ZXu4BJioHahTwsusqjYg",
+  authDomain: "react-auth-96cc9.firebaseapp.com",
+  projectId: "react-auth-96cc9",
+  storageBucket: "react-auth-96cc9.appspot.com",
+  messagingSenderId: "139566169306",
+  appId: "1:139566169306:web:8d97bd8e6d4de7ca0f79e3",
+  measurementId: "G-VSLQNHT2L2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
